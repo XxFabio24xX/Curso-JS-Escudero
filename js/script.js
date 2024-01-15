ecfe3d3062d85aafe4de6189f53d8c155b996823
Fabio Escudero <fabioescudero14@gmail.com>
2024-01-15 19:57:51 -0300

    Agrego un poco de estilo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//ctrl + K + C para comentar lineas
+//ctrl + K + U para descomentar lineas
+
 //Solicito nombre del cliente
 let nombreYapellido = prompt("Bienvenido a MDZ Car Rent \nIngrese su nombre y apellido")
 
@@ -5,14 +8,14 @@ let opcion = prompt("Elige una opcion de auto para rentar: \n1-Auto Gama Baja \n
 
 while(opcion != "X" && opcion != "x"){
     switch(opcion){
-        case "1": let dias = prompt("Cuantos dias desea rentar el auto? \nValor por dia: $20.000")
-            alert("Alquilar un auto de Gama Baja por "+dias+" dias va a costar: $"+costo(dias,opcion));
+        case "1": let dias = parseInt(prompt("Cuantos dias desea rentar el auto? \nValor por dia: $20.000")) 
+            alert("Alquilar un auto de Gama Baja por " + dias + " días va a costar: $" + costo(dias, opcion));
             break
         case "2": let dias2 = prompt("Cuantos dias desea rentar el auto? \nValor por dia: $30.000")
-            alert("Alquilar un auto de Gama Media por "+dias2+" dias va a costar: $"+costo(dias2,opcion));
+            alert("Alquilar un auto de Gama Media por " + dias2+" dias va a costar: $" + costo(dias2,opcion));
             break
         case "3": let dias3 = prompt("Cuantos dias desea rentar el auto? \nValor por dia: $40.000")
-            alert("Alquilar un auto de Gama Alta por "+dias3+" dias va a costar: $"+costo(dias3,opcion));
+            alert("Alquilar un auto de Gama Alta por " + dias3 + " dias va a costar: $" + costo(dias3,opcion));
             break
     }
     opcion = prompt(
